20e405248d9ac2e5ab73ad262e5346ccee5176c8
schleyfox <schleyfox@f4d61c19-7e91-46bd-81a1-9778e9c165bd>
2008-04-02 02:15:46 +0000

    Edited peach_test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/peach/trunk@4 f4d61c19-7e91-46bd-81a1-9778e9c165bd

--- peach_test.rb
+++ peach_test.rb
@@ -13,7 +13,7 @@ end
 puts "PEACH TEST"
 puts "each:"
 def each_test
-  (0...1000).to_a.each do |x|
+  (0...1000).to_a.sort_by{rand}.each do |x|
     fac(x)
   end
 end
@@ -21,7 +21,7 @@ puts Benchmark.measure { each_test }
 
 puts "peach:"
 def peach_test
-  (0...1000).to_a.peach(4) do |x|
+  (0...1000).to_a.sort_by{rand}.peach(4) do |x|
     fac(x)
   end
 end
@@ -29,7 +29,7 @@ puts Benchmark.measure { peach_test }
 
 puts "map:"
 def map_test
-  (0...1000).to_a.map do |x|
+  (0...1000).to_a.sort_by{rand}.map do |x|
     fac(x)
   end
 end
@@ -38,7 +38,7 @@ puts Benchmark.measure { map_test }
 
 puts "pmap:"
 def pmap_test
-  (0...1000).to_a.pmap(4) do |x|
+  (0...1000).to_a.sort_by{rand}.pmap(4) do |x|
     fac(x)
   end
 end
