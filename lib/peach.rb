5ffbfd9987137c0041960bc8eae98df4039920c9
Magnus Morton <magnusmorton@gmail.com>
2012-05-07 01:02:23 +0100

    removed 'divvy' and its tests

--- lib/peach.rb
+++ lib/peach.rb
@@ -44,36 +44,6 @@ module Peach
     result
   end
 
-
-
-  protected
-  def peach_run(meth, b, n = nil)
-    threads, results, result = [],[],[]
-    peach_divvy(n).each_with_index do |x,i|
-      threads << Thread.new { results[i] = x.send(meth, &b)}
-    end
-    threads.each {|t| t.join }
-    results.each {|x| result += x if x}
-    result
-  end
-  
-  def peach_divvy(n = nil)
-    return [] if size == 0
-
-    n ||= $peach_default_threads || size
-    n = size if n > size
-
-    lists = []
-
-    div = (size/n).floor
-    offset = 0
-    for i in (0...n-1)
-      lists << slice(offset, div)
-      offset += div
-    end
-    lists << slice(offset...size)
-    lists
-  end
 end
 
 Array.send(:include, Peach)
--- test/peach_test.rb
+++ test/peach_test.rb
@@ -38,39 +38,4 @@ class PeachTest < Test::Unit::TestCase
     end
   end
 
-  context "divvy" do
-    setup do
-      @data = [1, 2, 3, 4, 5]
-    end
-
-    context "on empty list" do
-      should "return empty list" do
-        assert_equal [], [].send(:peach_divvy)
-      end
-    end
-
-    context "when n is nil" do
-      should "put 1 element into each division" do
-        assert_equal @data.size, @data.send(:peach_divvy).size
-      end
-    end
-
-    context "when n is less than array size" do
-      should "put create n divisions" do
-        assert_equal 2, @data.send(:peach_divvy, 2).size
-      end
-
-      should "not lose any array elements" do
-        assert_equal @data.size, @data.send(:peach_divvy, 2).inject(0) {|sum, i|
-          sum + i.size
-        }
-      end
-    end
-
-    context "when n is greater than array size" do
-      should "only create 'array size' divisions" do
-        assert_equal @data.size, @data.send(:peach_divvy, 42).size
-      end
-    end
-  end
 end
